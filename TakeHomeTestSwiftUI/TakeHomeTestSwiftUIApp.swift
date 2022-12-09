e9909240880686d18c93b5bd4936260eb674d49d
Ahmad Syarifuddin Randiko <ddikodroid@gmail.com>
2022-12-09 20:50:56 +0700

    Initial Commit

---
+++ TakeHomeTestSwiftUI/TakeHomeTestSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TakeHomeTestSwiftUIApp.swift
+//  TakeHomeTestSwiftUI
+//
+//  Created by Ahmad Syarifuddin Randiko on 09/12/22.
+//
+
+import SwiftUI
+
+@main
+struct TakeHomeTestSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
